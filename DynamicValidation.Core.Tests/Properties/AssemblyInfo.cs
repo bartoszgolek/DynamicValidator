94af034f49bd92b50731b02f308311fe69b2dda4
Bartosz Gołek <bartosz.golek@gmail.com>
2015-09-04 14:43:43 +0200

    Basic Test cases

--- DynamicValidation.Core.Tests/Properties/AssemblyInfo.cs
+++ DynamicValidation.Core.Tests/Properties/AssemblyInfo.cs
@@ -2,36 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DynamicValidation.Core")]
+[assembly: AssemblyTitle("DynamicValidation.Core.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DynamicValidation.Core")]
+[assembly: AssemblyProduct("DynamicValidation.Core.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("31685546-8817-4886-8e73-ea88c371419c")]
+[assembly: Guid("13d8dba7-d342-4973-a5e0-8c35c365512f")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("DynamicValidation.Core.Tests")]
---
+++ DynamicValidation.Core.Tests/ValidatorTest.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using NUnit.Framework;
+
+namespace DynamicValidation.Core.Tests
+{
+	[TestFixture]
+	public class ValidatorTest
+	{
+		private readonly Expression<Func<string, bool>> unvalidRule = s => false;
+		private readonly Expression<Func<string, bool>> validRule = s => true;
+
+		[Test]
+		public void ValidatorWithoutRulesWillReturnValidResult()
+		{
+			var validator = Validator.For<string>(builder => { });
+			var result = validator.Validate(null);
+
+			Assert.IsTrue(result.Result);
+		}
+
+		[Test]
+		public void ValidatorWithFalseRuleWillReturnUnvalidResult()
+		{
+			var validator = Validator.For<string>(builder => builder.RuleOn(s => s).Expression(unvalidRule));
+			var result = validator.Validate(null);
+
+			Assert.IsFalse(result.Result);
+		}
+
+		[Test]
+		public void ValidatorWithValidRuleWillReturnValidResult()
+		{
+			var validator = Validator.For<string>(builder => builder.RuleOn(s => s).Expression(validRule));
+			var result = validator.Validate(null);
+
+			Assert.IsTrue(result.Result);
+		}
+
+		[Test]
+		public void ValidatorWithValidRuleWillReturnResultWithValidDetail()
+		{
+			var validator = Validator.For<string>(builder => builder.RuleOn(s => s).Expression(validRule));
+			var result = validator.Validate(null);
+
+			Assert.IsTrue(result.Details.First().Result);
+		}
+
+		[Test]
+		public void ValidatorWithUnvalidRuleWillReturnResultWithUnvalidDetail()
+		{
+			var validator = Validator.For<string>(builder => builder.RuleOn(s => s).Expression(unvalidRule));
+			var result = validator.Validate(null);
+
+			Assert.IsFalse(result.Details.First().Result);
+		}
+
+		[Test]
+		public void ValidatorWithValidRuleWillNotReturnMessageInRuleResult()
+		{
+			var validator = Validator.For<string>(builder => builder.RuleOn(s => s).Expression(validRule));
+			var result = validator.Validate(null);
+
+			Assert.AreEqual(String.Empty, result.Details.First().Message);
+		}
+
+		[Test]
+		public void ValidatorWithValidRuleWithoutMessageWillReturnDefaultMessageInRuleResult()
+		{
+			var validator = Validator.For<string>(builder => builder.RuleOn(s => s).Expression(unvalidRule));
+			var result = validator.Validate(null);
+
+			Assert.AreEqual("s => False", result.Details.First().Message);
+		}
+
+		[Test]
+		public void ValidatorWithValidRuleWithoutMessageWillReturnDefaultMessageWithPropertyNameWhenMatchedForPropertyInRuleResult()
+		{
+			var validator = Validator.For<SimpleTestEntity>(builder => builder.RuleOn(te => te.S).Expression(unvalidRule));
+			var result = validator.Validate(new SimpleTestEntity());
+
+			Assert.AreEqual("S: s => False", result.Details.First().Message);
+		}
+
+		[Test]
+		public void ValidatorWithValidRuleWithoutMessageWillReturnCustomMessageWhenMessageIsSetInRuleResult()
+		{
+			var validator = Validator.For<SimpleTestEntity>(builder =>
+				builder.RuleOn(te => te.S).Expression(unvalidRule).Message("custom")
+			);
+			var result = validator.Validate(new SimpleTestEntity());
+
+			Assert.AreEqual("custom", result.Details.First().Message);
+		}
+
+		[Test]
+		public void ValidatorWillSkipAndReturnValidResultForRuleWithFalseWhen()
+		{
+			var validator = Validator.For<SimpleTestEntity>(builder =>
+				builder.RuleOn(te => te.S).Expression(unvalidRule).Message("custom").When(s => false)
+			);
+			var result = validator.Validate(new SimpleTestEntity());
+
+			Assert.IsTrue(result.Details.First().Result);
+		}
+
+		private class SimpleTestEntity
+		{
+			public string S { get; set; }
+		}
+	}
+}
